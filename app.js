377e693876cd14f0c9fb7c678b8a6654e5a0aef9
Ankita Durgavajula <35971200+ad474@users.noreply.github.com>
2019-12-15 18:40:11 +0530

    integrated template with backend

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+//app.use(express.static(path.join(__dirname, "js")));
 
 app.get("/", function(req,res){
-  res.render("index");
+  res.sendFile('views/index.html' , { root : __dirname});
+  //res.render("index");
 });
 
 app.listen(3000, function(){
