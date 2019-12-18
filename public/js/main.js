088aef05d3e1c5872a5290b4823de524106be1dc
Dev Kathuria <dk382@snu.edu.in>
2019-12-19 00:41:45 +0530

    form upload complete

--- public/js/main.js
+++ public/js/main.js
@@ -147,7 +147,6 @@ var kursorx = new kursor({
   type: 1,
   color: 'rgb(183,59,42)',
   removeDefaultCursor: true
-})
 });
 
 
@@ -267,5 +266,154 @@ particlesJS("particles-js", {
 });
 
 
+  if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+  firebase.initializeApp({
+    "projectId": "hackdata-3db9b",
+    "appId": "1:405958797889:web:2b95d7d48fe59b2cdeb4c0",
+    "databaseURL": "https://hackdata-3db9b.firebaseio.com",
+    "storageBucket": "hackdata-3db9b.appspot.com",
+    "locationId": "asia-northeast1",
+    "apiKey": "AIzaSyAIk17fkMiY9nWF_2i5I1nyUiFo4acrkAA",
+    "authDomain": "hackdata-3db9b.firebaseapp.com",
+    "messagingSenderId": "405958797889"
+  });
+
+const db=firebase.firestore();
+  var link1,link2,link3;
+
+  const form1 = document.getElementById("myform");
+  if (form1) {
+    form1.addEventListener('submit', function (e) {
+      e.preventDefault();
+      upfile(form1.teamname.value);
+    });
+  }
+  var res1,res2,res3;
+  $('#filename').on("change", function(event){
+    res1 = event.target.files[0];
+  });
+
+  $('#filename2').on("change", function(event){
+    res2 = event.target.files[0];
+  });
+
+  $('#filename3').on("change", function(event){
+    res3 = event.target.files[0];
+  });
+
+function upfile(teamname){
+  var filename = res1.name;
+  var storageRef  = firebase.storage().ref('/resumes/'+teamname+'/' + Date.now()+'1'+ filename);
+  var uploadTask  = storageRef.put(res1);
+  // Register three observers:
+// 1. 'state_changed' observer, called any time the state changes
+// 2. Error observer, called on failure
+// 3. Completion observer, called on successful completion
+  uploadTask.on('state_changed', function(snapshot){
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case firebase.storage.TaskState.PAUSED: // or 'paused'
+        console.log('Upload is paused');
+        break;
+      case firebase.storage.TaskState.RUNNING: // or 'running'
+        console.log('Upload is running');
+        break;
+    }
+  }, function(error) {
+    // Handle unsuccessful uploads
+  }, function() {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+      console.log('File available at', downloadURL);
+      link1 = downloadURL;
+      upfile2(teamname);
+    });
+  });
+
+}
+
+  function upfile2(teamname){
+    var filename2 = res2.name;
+    var storageRef2 = firebase.storage().ref('/resumes/'+teamname+'/' + Date.now()+'2' + filename2);
+    var uploadTask2 = storageRef2.put(res2);
+
+    uploadTask2.on('state_changed', function(snapshot){
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, function(error) {
+      // Handle unsuccessful uploads
+    }, function() {
+      // Handle successful uploads on complete
+      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      uploadTask2.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+        console.log('File available at', downloadURL);
+        link2 = downloadURL;
+        upfile3(teamname);
+      });
+    });
+
+  }
 
 
+  function upfile3(teamname){
+    var filename3 = res3.name;
+    var storageRef3 = firebase.storage().ref('/resumes/'+teamname+'/' + Date.now()+'3' + filename3);
+
+    var uploadTask3 = storageRef3.put(res3);
+
+
+    uploadTask3.on('state_changed', function(snapshot){
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, function(error) {
+      // Handle unsuccessful uploads
+    }, function() {
+      // Handle successful uploads on complete
+      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      uploadTask3.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+        console.log('File available at', downloadURL);
+        link3 = downloadURL;
+        updateDb();
+      });
+    });
+
+
+  }
+
+
+function updateDb() {
+  console.log('written');
+  db.collection('applicants').add({
+    teamname: form1.teamname.value,
+    resume1: link1+'',
+    resume2: link2+'',
+    resume3: link3+'',
+  });
+}
+
+
+});
