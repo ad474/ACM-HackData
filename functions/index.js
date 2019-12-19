6e8cb1d522b2a8f8db03a24eb386f42c0f29a181
Ankita Durgavajula <35971200+ad474@users.noreply.github.com>
2019-12-19 20:54:20 +0530

    without mailer

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,20 @@
 const functions = require('firebase-functions');
+//const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+// Add middleware to authenticate requests
+//app.use(myMiddleware);
+
+exports.date = functions.https.onRequest((req, res) => {
+  res.send("okay");
+});
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
