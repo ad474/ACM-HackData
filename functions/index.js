39ab839b67d4a1214237080833c7bc0cbc70bb7b
Dev Kathuria <dk382@snu.edu.in>
2019-12-21 18:23:35 +0530

    front end validation firebase deploy captcha added(backend left) Intuitive file dialog box

--- functions/index.js
+++ functions/index.js
@@ -13,7 +13,7 @@ app.use(cors({ origin: true }));
 
 exports.date = functions.https.onRequest((req, res) => {
   res.send("okay");
-});
+})
 
 
 // // Create and Deploy Your First Cloud Functions
@@ -22,3 +22,36 @@ exports.date = functions.https.onRequest((req, res) => {
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+/*
+//CAPTCHA
+const functions = require('firebase-functions')
+const rp = require('request-promise')
+
+exports.checkRecaptcha = functions.https.onRequest((req, res) => {
+  const response = req.query.response
+  console.log("recaptcha response", response)
+  rp({
+    uri: 'https://recaptcha.google.com/recaptcha/api/siteverify',
+    method: 'POST',
+    formData: {
+      secret: '6LeOCskUAAAAAAkz5cT1SoMO7iVmTvOtt8xiKdVb',
+      response: response
+    },
+    json: true
+  }).then(result => {
+    console.log("recaptcha result", result)
+    if (result.success) {
+      res.send("You're good to go, human.")
+    }
+    else {
+      res.send("Recaptcha verification failed. Are you a robot?")
+    }
+  }).catch(reason => {
+    console.log("Recaptcha request failure", reason);
+    res.send("Recaptcha request failed.")
+  })
+
+
+ */
+//})
+
